8a0537068fa0cfc063fbebc1efb2e9c648e5a1bb
Vítor Ferreira <42324938+vitorfn@users.noreply.github.com>
2020-06-08 18:18:33 -0300

    :pencil2: Criação das Classes A e B

---
+++ classeA.cpp
@@ -0,0 +1,41 @@
+class A{
+   private:
+      int A1;
+      float A2;
+   public:
+      int getA1();
+      void setA1(int valor1);
+      float getA2();
+      void setA2(float valor2);
+      void MA1();
+      void MA2();
+};
+
+A::A(){
+  A1 = 0;
+  A2 = 0;
+}
+
+int A::getA1(){
+  return A1;
+}
+
+void A::setA1(int valor1){
+  A1 = valor1;
+}
+
+float A::getA2(){
+  return A2;
+}
+
+void A::setA2(float valor2){
+  A2 = valor2;
+}
+
+void A::MA1(){
+  cout << A1 << endl;
+}
+
+void A::MA2(){
+  cout << A2 << endl;
+}
---
+++ classeB.cpp
@@ -0,0 +1,41 @@
+class B{
+   private:
+      int B1;
+      float B2;
+   public:
+      int getB1();
+      void setB1(int valor1);
+      float getB2();
+      void setB2(float valor2);
+      void MB1();
+      void MB2();
+};
+
+B::B(){
+  B1 = 0;
+  B2 = 0;
+}
+
+int B::getB1(){
+  return B1;
+}
+
+void B::setB1(int valor1){
+  B1 = valor1;
+}
+
+float B::getB2(){
+  return B2;
+}
+
+void B::setB2(float valor2){
+  B2 = valor2;
+}
+
+void B::MB1(){
+  cout << B1 << endl;
+}
+
+void B::MB2(){
+  cout << B2 << endl;
+}
